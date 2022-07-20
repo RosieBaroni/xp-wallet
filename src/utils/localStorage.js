bc967b4b8bd77d3ade5d6bf32d39819c0b870aa2
RosieBaroni <rosi.baroni@gmail.com>
2022-07-19 21:31:35 -0300

    feat: add new localStorage methods utils

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -2,4 +2,10 @@ export const saveEmail = (email) => {
   localStorage.setItem('user', JSON.stringify({ email }));
 };
 
+export const saveUserStocks = (stocks) => {
+  localStorage.setItem('stocks', JSON.stringify(stocks));
+}
+
+export const getUserStocks = () => JSON.parse(localStorage.getItem('stocks')) || [];
+
 export const getEmail = () => JSON.parse(localStorage.getItem('user'));
