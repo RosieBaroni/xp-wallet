614f0febbe9f67480219fee374a676b7ed407497
RosieBaroni <rosi.baroni@gmail.com>
2022-07-22 07:00:06 -0300

    feat: implements currency formatting and new navigate on wallet page

--- src/pages/Wallet/index.js
+++ src/pages/Wallet/index.js
@@ -5,6 +5,7 @@ import Header from '../../components/Header';
 import Table from '../../components/Table';
 import stocks from '../../data/stocks.json'
 import { getUserStocks } from '../../utils/localStorage'
+import { formatCurrencyToBRL } from '../../utils/currency';
 
 function Wallet() {
   const userStocks = getUserStocks();
@@ -15,7 +16,7 @@ function Wallet() {
   const columns = [
     'Ação',
     'Quantidade',
-    'Valor (R$)',
+    'Valor unitário',
   ];
 
   const navigate = useNavigate();
@@ -34,12 +35,12 @@ function Wallet() {
           {stock.amount || 1}
         </td>
         <td>
-          {stock.price}
+          {formatCurrencyToBRL(stock.price)}
         </td>
         {stock.amount ?
           (
             <td>
-              {(stock.price * stock.amount).toFixed(2)}
+              {formatCurrencyToBRL(stock.price * stock.amount)}
             </td>
           ) : null
         }
@@ -66,15 +67,43 @@ function Wallet() {
     ))
   }
 
+  const goBack = () => {
+    navigate('/');
+  }
+
+  const goBalance = () => {
+    navigate('/balance');
+  }
+
   return (
     <div>
       <Header />
 
       <h2>Minhas Ações:</h2>
-      <Table columns={[...columns, 'Valor total (R$)', 'Negociar']} renderRows={() => renderStocks(userStocks)} />
+      <Table columns={
+        [...columns, 'Valor total', 'Negociar']}
+        renderRows={() => renderStocks(userStocks)}
+      />
 
       <h2>Disponíveis para investir:</h2>
-      <Table columns={[...columns, 'Negociar']} renderRows={() => renderStocks(availableStocks)} />
+      <Table columns={
+        [...columns, 'Negociar']}
+        renderRows={() => renderStocks(availableStocks)}
+      />
+
+      <button
+        type="button"
+        onClick={goBack}
+      >
+        Voltar
+      </button>
+
+      <button
+        type="button"
+        onClick={goBalance}
+      >
+        Depósito/Retirada
+      </button>
     </div>
   )
 }
