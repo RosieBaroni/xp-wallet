e83ff174ec0acc2f4d45bbc48c08e382813170f9
RosieBaroni <rosi.baroni@gmail.com>
2022-07-23 16:47:49 -0300

    refactor: table styles

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -4,21 +4,19 @@ import './styles.css';
 
 function Table({ columns, renderRows }) {
   return (
-    <div class="container-table">
-      <table>
-        <thead>
-          <tr>
-            {columns.map((column) => (
-              <th key={column}>{column}</th>
-            ))}
-          </tr>
-        </thead>
+    <table className="table">
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th key={column}>{column}</th>
+          ))}
+        </tr>
+      </thead>
 
-        <tbody>
-          {renderRows()}
-        </tbody>
-      </table >
-    </div>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
   )
 }
 
